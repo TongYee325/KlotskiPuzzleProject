24015571944379c4e1486527194d0564894587df
tongyee <12313533@mail.sustech.edu.cn>
2025-04-14 16:42:55 +0800

    初始化

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import level.LoginLevel;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        LoginLevel loginLevel = new LoginLevel();
+        LoginLevel loginlevel = new LoginLevel();
     }
 }
